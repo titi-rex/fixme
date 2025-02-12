7f97b060cb1bc1ee13923d859cc35ceeea034932
reina <109018113+titi-rex@users.noreply.github.com>
2025-02-12 17:24:22 +0100

    init project

---
+++ broker/src/main/java/fr/tlegrand/broker/Broker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.tlegrand.broker;
+
+/**
+ *
+ * @author reina
+ */
+public class Broker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ market/src/main/java/fr/tlegrand/market/Market.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.tlegrand.market;
+
+/**
+ *
+ * @author reina
+ */
+public class Market {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
